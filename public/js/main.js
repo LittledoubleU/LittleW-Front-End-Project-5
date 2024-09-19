a4164b7a9c8118481277b26872de3d119c9a7d86
LittleW <w-army@hotmail.co.th>
2024-09-19 18:16:33 +0700

    completed aniamtion part???

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,14 @@
-// var textArray = $('.text').map(function() {
-//     return $(this).text();
-// }).get();
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry); // Check if entry is visible
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-// textArray.forEach((element, index) => {
-//     let textDiv = $('.text').eq(index); // Get the corresponding .text div
-//     let width = textDiv.width(); // Get width of the .text div
-//     let height = textDiv.height(); // Get height of the .text div
-    
-//     // Assuming you are working with an SVG text element that has a getBBox method
-//     let bb = textDiv.getBBox();
-//     let widthTransform = width / bb.width;
-//     let heightTransform = height / bb.height;
-//     let value = widthTransform < heightTransform ? widthTransform : heightTransform;
-//     textDiv.setAttribute("transform", "matrix("+value+", 0, 0, "+value+", 0,0)");
+const hiddenElements = $('section');
 
-//     // Append to the document or SVG container
-//     // svg.appendChild(textDiv); // Example if you are appending to an SVG element
-// });
+hiddenElements.each((index, el) => observer.observe(el));
